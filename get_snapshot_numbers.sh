9653d8ae35ae31ab6e5a7f1052ed7a6cbb6ee4d7
Eric Berquist <eric.berquist@gmail.com>
2015-10-16 15:54:33 -0400

    Split up the long line and add a comment.

--- get_snapshot_numbers.sh
+++ get_snapshot_numbers.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env sh
 
-find . -type f -print0 | xargs -0 -n 1 basename | cut -d "_" -f 2 | sort | uniq
+# get_snapshot_numbers.sh: run inside of a directory with calculation
+# inputs to get a sorted list of snapshot numbers
+
+find . -type f -name "*drop*.in" -print0 | \
+    xargs -0 -n 1 basename | \
+    cut -d "_" -f 2 | \
+    sort | \
+    uniq
