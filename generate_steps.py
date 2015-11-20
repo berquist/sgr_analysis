38908415b8e9a1c996e8025f949e1e7a3d176c30
Eric Berquist <eric.berquist@gmail.com>
2015-11-20 11:05:05 -0500

    Make RHF, UHF singlet, and UHF triplet scan inputs.

--- generate_steps.py
+++ generate_steps.py
@@ -5,7 +5,7 @@ from __future__ import print_function
 import numpy as np
 
 
-def template(coord_O1, coord_O2, coord_C=0.0000000000):
+def qchem_template_restricted(coord_O1, coord_O2, coord_C=0.0000000000):
 
     return """$molecule
 0 1
@@ -33,6 +33,37 @@ $end
 """.format(**locals())
 
 
+def qchem_template_unrestricted(coord_O1, coord_O2, coord_C=0.0000000000, mult=1):
+
+    return """$molecule
+0 {mult}
+C         0.0000000000   {coord_C:>13.10f}    0.0000000000
+O         0.0000000000    0.0000000000   {coord_O1:>13.10f}
+O         0.0000000000    0.0000000000   {coord_O2:>13.10f}
+$end
+
+$rem
+SYMMETRY = FALSE
+SYM_IGNORE = TRUE
+BASIS  =  6-311++G(d,p)
+EXCHANGE  =  B3LYP
+JOB_TYPE  =  FREQ
+SCF_CONVERGENCE  =  8
+SCF_ALGORITHM = RCA_DIIS
+THRESH  =  12
+XC_SMART_GRID = True
+XC_GRID = 000099000590
+MAX_SCF_CYCLES = 10000
+MAX_RCA_CYCLES = 1000
+THRESH_RCA_SWITCH = 6
+FAST_XC = TRUE
+UNRESTRICTED = TRUE
+SCF_GUESS_MIX = TRUE
+SCF_GUESS = GWH
+$end
+""".format(**locals())
+
+
 def make_steps(stepsize, stepmin=-0.5, stepmax=10.0):
     steps = np.arange(stepmin, stepmax + stepsize, stepsize)
     # I hate floating point
@@ -52,27 +83,34 @@ if __name__ == '__main__':
     parser.add_argument('--stepmin', type=float, default=-0.5)
     parser.add_argument('--stepmax', type=float, default=10.0)
     parser.add_argument('--stepsize', type=float, default=0.025)
+    parser.add_argument('--mult', type=int, default=1)
+    parser.add_argument('--unrestricted', action='store_true')
 
     args = parser.parse_args()
+
+    if args.unrestricted:
+        qchem_template = qchem_template_unrestricted
+    else:
+        qchem_template = qchem_template_restricted
 
     steps = make_steps(args.stepsize, stepmin=args.stepmin, stepmax=args.stepmax)
     print(steps)
 
-    start = -1.169160
+    start = -1.160791
 
     if args.kind == 'stretch_1':
         for count, step in enumerate(steps, start=1):
-            input_file_contents = template(start, -start + step)
+            input_file_contents = qchem_template(start, -start + step)
             with open('step_{}.in'.format(count), 'w') as input_file:
                 input_file.write(input_file_contents)
     elif args.kind == 'stretch_2':
         for count, step in enumerate(steps, start=1):
-            input_file_contents = template(start - step, -start + step)
+            input_file_contents = qchem_template(start - step, -start + step)
             with open('step_{}.in'.format(count), 'w') as input_file:
                 input_file.write(input_file_contents)
     elif args.kind == 'bend':
         for count, step in enumerate(steps, start=1):
-            input_file_contents = template(coord_O1=start,
+            input_file_contents = qchem_template(coord_O1=start,
                                            coord_O2=-start,
                                            coord_C=0.0 + step)
             with open('step_{}.in'.format(count), 'w') as input_file:
