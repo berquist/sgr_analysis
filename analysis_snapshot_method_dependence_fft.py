3b9ff6713b55ac0a5eb5bc9a2b619a44e5062d11
Eric Berquist <eric.berquist@gmail.com>
2016-02-02 11:51:48 -0500

    Preliminary work on dipole ACF-based IR spectrum.

--- analysis_snapshot_method_dependence_fft.py
+++ analysis_snapshot_method_dependence_fft.py
@@ -12,9 +12,13 @@ from qchem_aimd_fft import \
 import pickle
 
 import numpy as np
+import scipy.stats as sps
 
 # from parse_outputs_snapshot_method_dependence_4fs import \
 #     (methods, basis_sets)
+
+from analysis_histograms import \
+    (make_bin_edges, norm_pdf)
 
 import matplotlib as mpl
 mpl.use("Agg")
@@ -23,8 +27,12 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
+    with open('frequencies.pypickle', 'rb') as picklefile:
+        frequencies = pickle.load(picklefile)
     with open('dipoles.pypickle', 'rb') as picklefile:
         dipoles = pickle.load(picklefile)
+    with open('snapnums_frequencies.pypickle', 'rb') as picklefile:
+        snapnums_f = pickle.load(picklefile)
     with open('snapnums_dipoles.pypickle', 'rb') as picklefile:
         snapnums_d = pickle.load(picklefile)
 
@@ -50,15 +58,105 @@ if __name__ == '__main__':
 
     #         plt.close('all')
 
-    dipoles_b3lyp_lp_0_0 = np.array(dipoles['flex']['b3lyp'][''][0][0])
-    frequencies, intensities = make_vibspectrum_from_dipoles(dipoles_b3lyp_lp_0_0, time_step)
-    assert frequencies.shape == intensities.shape
-    print(frequencies.shape)
+    # these are the raw frequencies; I want to plot the
+    # distribution/histogram
+    frequencies_flex_b3lyp_lp_0_0 = np.array(frequencies['flex']['b3lyp']['lp'][0][0])
+    frequencies_rigid_b3lyp_lp_0_0 = np.array(frequencies['rigid']['b3lyp']['lp'][0][0])
+    dipoles_flex_b3lyp_lp_0_0 = np.array(dipoles['flex']['b3lyp']['lp'][0][0])
+    # dipoles_rigid_b3lyp_lp_0_0 = np.array(dipoles['rigid']['b3lyp']['lp'][0][0])
+    frequencies_flex, intensities_flex = make_vibspectrum_from_dipoles(dipoles_flex_b3lyp_lp_0_0, time_step)
+    # frequencies_rigid, intensities_rigid = make_vibspectrum_from_dipoles(dipoles_rigid_b3lyp_lp_0_0, time_step)
+    assert frequencies_flex.shape == intensities_flex.shape
+    # assert frequencies_rigid.shape == intensities_rigid.shape
+    # print(frequencies_flex.shape, frequencies_rigid.shape)
+
+    ## Plot the IR spectrum for the flexible and rigid snapshots as
+    ## the FFT of the dipole autocorrelation function.
+
+    wavenumber_cutoff_lo = 2420.0
+    wavenumber_cutoff_hi = 2480.0
+    # wavenumber_cutoff_lo = 0.0
+    # wavenumber_cutoff_hi = 5000.0
+
+    mask_lo = frequencies_flex >= wavenumber_cutoff_lo
+    mask_hi = frequencies_flex <= wavenumber_cutoff_hi
+    mask = np.logical_and(mask_lo, mask_hi)
+    ix = np.where(mask)[0]
 
     fig, ax = plt.subplots()
 
-    ax.plot(frequencies, intensities, linewidth=0.50)
+    ax.plot(frequencies_flex[ix], intensities_flex[ix], label='flex', linewidth=0.50)
+    # ax.plot(frequencies_rigid[ix], intensities_rigid[ix], label='rigid', linewidth=0.50)
+
+    ax.set_xlabel('frequency (cm$^{-1}$)')
+    ax.set_ylabel('intensity (a.u.)')
+
+    ax.legend(loc='best', fancybox=True, framealpha=0.50)
 
     fig.savefig('fft_{}_{}.pdf'.format('b3lyp', 'lp'), bbox_inches='tight')
+
+    plt.close('all')
+
+    ## Plot the distribution of \nu_3 harmonic frequencies for the
+    ## flexible and rigid snapshots.
+
+    numbins = 10
+
+    center_flex = np.mean(frequencies_flex_b3lyp_lp_0_0)
+    center_rigid = np.mean(frequencies_rigid_b3lyp_lp_0_0)
+    # standard deviation of the population, not sample!
+    binwidth_flex = np.std(frequencies_flex_b3lyp_lp_0_0, ddof=1)
+    binwidth_rigid = np.std(frequencies_rigid_b3lyp_lp_0_0, ddof=1)
+    bins_flex = make_bin_edges(numbins, center_flex, binwidth_flex)
+    bins_rigid = make_bin_edges(numbins, center_rigid, binwidth_rigid)
+    hist_flex, bin_edges_flex = np.histogram(frequencies_flex_b3lyp_lp_0_0, bins_flex)
+    hist_rigid, bin_edges_rigid = np.histogram(frequencies_rigid_b3lyp_lp_0_0, bins_rigid)
+    linspace_bins_flex = np.linspace(bins_flex[0], bins_flex[-1], 300)
+    linspace_bins_rigid = np.linspace(bins_rigid[0], bins_rigid[-1], 300)
+    pdf_bins_flex = norm_pdf(linspace_bins_flex, mu=center_flex, sigma=binwidth_flex)
+    pdf_bins_rigid = norm_pdf(linspace_bins_rigid, mu=center_rigid, sigma=binwidth_rigid)
+
+    mmin = min(frequencies_flex_b3lyp_lp_0_0)
+    mmax = max(frequencies_flex_b3lyp_lp_0_0)
+    rng = mmax - mmin
+    mean = np.mean(frequencies_flex_b3lyp_lp_0_0)
+    median = np.median(frequencies_flex_b3lyp_lp_0_0)
+    mode = sps.mode(frequencies_flex_b3lyp_lp_0_0)
+    print(' min           : {:.2f}'.format(mmin))
+    print(' max           : {:.2f}'.format(mmax))
+    print(' range         : {:.2f}'.format(rng))
+    print(' mean          : {:.3f}'.format(mean))
+    print(' median        : {:.3f}'.format(median))
+    print(' mode          : {}'.format(mode))
+    print(' stdev (pop)   : {:.3f}'.format(np.std(frequencies_flex_b3lyp_lp_0_0, ddof=1)))
+    print(' stdev (sample): {:.3f}'.format(np.std(frequencies_flex_b3lyp_lp_0_0, ddof=0)))
+
+
+    fig, ax = plt.subplots()
+
+    ax.hist(frequencies_flex_b3lyp_lp_0_0, bins=bins_flex, normed=True, label='flex hist')
+    # ax.hist(frequencies_rigid_b3lyp_lp_0_0, bins=bins_rigid, normed=True, label='rigid hist')
+    ax.plot(linspace_bins_flex, pdf_bins_flex, label='flex PDF', linewidth=0.50, linestyle='--')
+    # ax.plot(linspace_bins_rigid, pdf_bins_rigid, label='rigid PDF', linewidth=0.50, linestyle='--')
+
+    ax.set_xlabel('harmonic frequency (cm$^{-1}$)')
+    # ax.set_ylabel('')
+
+    ax.legend(loc='best', fancybox=True, framealpha=0.50)
+
+    fig.savefig('harmonic_distribution_{}_{}.pdf'.format('b3lyp', 'lp'), bbox_inches='tight')
+
+    plt.close('all')
+
+    fig, ax = plt.subplots()
+
+    # for this to work, all the data should be normalized to something (?)
+    ax.plot(frequencies_flex[ix], intensities_flex[ix], label='flex dip-dip', linewidth=0.50)
+    ax.hist(frequencies_flex_b3lyp_lp_0_0, bins=bins_flex, normed=True, label='flex hist')
+    ax.plot(linspace_bins_flex, pdf_bins_flex, label='flex PDF', linewidth=0.50, linestyle='--')
+
+    ax.legend(loc='best', fancybox=True, framealpha=0.50)
+
+    fig.savefig('combined_{}_{}.pdf'.format('b3lyp', 'lp'), bbox_inches='tight')
 
     plt.close('all')
