f452aa55c221eb070c5524ce9aef44b2ea075b5f
Eric Berquist <eric.berquist@gmail.com>
2016-03-21 14:53:25 -0400

    box size analysis

---
+++ snapshots_calculate_box_size.py
@@ -0,0 +1,109 @@
+from __future__ import division
+from __future__ import print_function
+
+import csv
+import os
+
+import subprocess as sp
+
+import numpy as np
+
+from mbe.xyz_operations import read_xyz
+
+from mbe.examples.droplet import \
+    (determine_fragment_grouping, make_fragments_from_grouping,
+     fragment_centerofmass,
+     get_n_closest_fragments, distance_twopoint)
+
+
+if __name__ == '__main__':
+
+    find_output = sp.check_output('find /home/eric/Chemistry/calc.sgr/paper_02_CD_SC/NewXYZFiles/ -name "*.xyz" | sort', shell=True).decode()
+    xyzfilenames = find_output.splitlines()
+    dim = 1001
+    assert len(xyzfilenames) == dim
+
+    possible_n_qm = list(range(1, 6 + 1))
+    dropnums = {n_qm: [] for n_qm in possible_n_qm}
+    n_qm_to_distance_from_COM_COM_combined = {n_qm: [] for n_qm in possible_n_qm}
+    n_qm_to_distance_from_COM_COM_cation = {n_qm: [] for n_qm in possible_n_qm}
+    n_qm_to_distance_from_COM_COM_anion = {n_qm: [] for n_qm in possible_n_qm}
+
+    for xyzfilename in xyzfilenames:
+        dropnum = os.path.splitext(xyzfilename.split('_')[-1])[0]
+        print(dropnum)
+        _, _, elements, coords = read_xyz(xyzfilename)
+        assert elements[-3:] == ['C', 'O', 'O']
+        grouping_anions, grouping_cations, grouping_CO2 = determine_fragment_grouping(elements)
+        fragments_anions = make_fragments_from_grouping(elements, coords, grouping_anions)
+        fragments_cations = make_fragments_from_grouping(elements, coords, grouping_cations)
+        fragment_CO2 = make_fragments_from_grouping(elements, coords, grouping_CO2)
+        fragments = fragments_anions + fragments_cations + fragment_CO2
+        assert len(fragment_CO2) == 1
+        fragment_CO2 = fragment_CO2[0]
+        for n_qm in possible_n_qm:
+            # Wouldn't it be nice if we didn't calculate the distance twice?
+            closest_cations = get_n_closest_fragments(n_qm, fragment_CO2, fragments_cations, method='centerofmass')
+            closest_anions = get_n_closest_fragments(n_qm, fragment_CO2, fragments_anions, method='centerofmass')
+            max_distance_from_COM_COM_combined = -1.0e30
+            max_distance_from_COM_COM_cation = -1.0e30
+            max_distance_from_COM_COM_anion = -1.0e30
+            COM_CO2 = fragment_centerofmass(fragment_CO2)
+            for fragment_cation in closest_cations:
+                COM_cation = fragment_centerofmass(fragment_cation)
+                max_distance_from_COM_COM_cation = max(max_distance_from_COM_COM_cation,
+                                                       distance_twopoint(COM_CO2, COM_cation))
+            for fragment_anion in closest_anions:
+                COM_anion = fragment_centerofmass(fragment_anion)
+                max_distance_from_COM_COM_anion = max(max_distance_from_COM_COM_anion,
+                                                      distance_twopoint(COM_CO2, COM_anion))
+            max_distance_from_COM_COM_combined = max(max_distance_from_COM_COM_cation,
+                                                     max_distance_from_COM_COM_anion)
+            dropnums[n_qm].append(dropnum)
+            n_qm_to_distance_from_COM_COM_combined[n_qm].append(max_distance_from_COM_COM_combined)
+            n_qm_to_distance_from_COM_COM_cation[n_qm].append(max_distance_from_COM_COM_cation)
+            n_qm_to_distance_from_COM_COM_anion[n_qm].append(max_distance_from_COM_COM_anion)
+
+    row_header = [
+        'dropnum',
+        'distances_COM_COM_combined',
+        'distances_COM_COM_cation',
+        'distances_COM_COM_anion',
+    ]
+    for n_qm in possible_n_qm:
+        csvfh = open('raw_distances_{}QM.csv'.format(n_qm), 'w')
+        csvwriter = csv.writer(csvfh)
+        csvwriter.writerow(row_header)
+        assert len(dropnums[n_qm]) == len(n_qm_to_distance_from_COM_COM_combined[n_qm]) == len(n_qm_to_distance_from_COM_COM_cation[n_qm]) == len(n_qm_to_distance_from_COM_COM_anion[n_qm])
+        dim = len(dropnums[n_qm])
+        n_qm_array = np.empty(shape=(dim, 4))
+        n_qm_array[:, 0] = dropnums[n_qm]
+        n_qm_array[:, 1] = n_qm_to_distance_from_COM_COM_combined[n_qm]
+        n_qm_array[:, 2] = n_qm_to_distance_from_COM_COM_cation[n_qm]
+        n_qm_array[:, 3] = n_qm_to_distance_from_COM_COM_anion[n_qm]
+        csvwriter.writerows(n_qm_array.tolist())
+        csvfh.close()
+
+    csvfh = open('mean_box_sizes.csv', 'w')
+    csvwriter = csv.writer(csvfh)
+    row_header = [
+        'n_qm',
+        'mean_COM_COM_combined',
+        'mean_COM_COM_cation',
+        'mean_COM_COM_anion',
+    ]
+    csvwriter.writerow(row_header)
+    print(row_header)
+    for n_qm in possible_n_qm:
+        mean_COM_COM_combined = np.mean(n_qm_to_distance_from_COM_COM_combined[n_qm])
+        mean_COM_COM_cation = np.mean(n_qm_to_distance_from_COM_COM_cation[n_qm])
+        mean_COM_COM_anion = np.mean(n_qm_to_distance_from_COM_COM_anion[n_qm])
+        row = [
+            n_qm,
+            mean_COM_COM_combined,
+            mean_COM_COM_cation,
+            mean_COM_COM_anion
+        ]
+        csvwriter.writerow(row)
+        print(row)
+    csvfh.close()
---
+++ snapshots_plot_box_size.py
@@ -0,0 +1,86 @@
+from __future__ import division
+from __future__ import print_function
+
+import csv
+import os
+
+import subprocess as sp
+
+import numpy as np
+
+if __name__ == '__main__':
+
+    possible_n_qm = list(range(1, 6 + 1))
+    dropnums = np.arange(1, 1001 + 1)
+    distances = dict()
+
+    for n_qm in possible_n_qm:
+        with open('raw_distances_{}QM.csv'.format(n_qm)) as csvfile:
+            csvreader = csv.reader(csvfile)
+            rows = []
+            header = next(csvreader)
+            assert header == [
+                'dropnum',
+                'distances_COM_COM_combined',
+                'distances_COM_COM_cation',
+                'distances_COM_COM_anion',
+            ]
+            for row in csvreader:
+                rows.append(row)
+        rows = np.array(sorted(rows), dtype=float)
+        distances[n_qm] = rows
+
+    for n_qm in possible_n_qm:
+        assert dropnums.all() == distances[n_qm][:, 0].all()
+        assert distances[n_qm].shape == (1001, 4)
+    dim = rows.shape[0]
+    distances_COM_COM_combined = np.empty(shape=(dim, len(possible_n_qm) + 1))
+    distances_COM_COM_cation = np.empty(shape=(dim, len(possible_n_qm) + 1))
+    distances_COM_COM_anion = np.empty(shape=(dim, len(possible_n_qm) + 1))
+    distances_COM_COM_combined[:, 0] = dropnums
+    distances_COM_COM_cation[:, 0] = dropnums
+    distances_COM_COM_anion[:, 0] = dropnums
+    for n_qm in possible_n_qm:
+        distances_COM_COM_combined[:, n_qm] = distances[n_qm][:, 1]
+        distances_COM_COM_cation[:, n_qm] = distances[n_qm][:, 2]
+        distances_COM_COM_anion[:, n_qm] = distances[n_qm][:, 3]
+
+    means_combined = [np.mean(distances_COM_COM_combined[:, n_qm]) for n_qm in possible_n_qm]
+    means_cation = [np.mean(distances_COM_COM_cation[:, n_qm]) for n_qm in possible_n_qm]
+    means_anion = [np.mean(distances_COM_COM_anion[:, n_qm]) for n_qm in possible_n_qm]
+    print(means_combined)
+    print(means_cation)
+    print(means_anion)
+
+    import matplotlib as mpl
+    mpl.use('Agg')
+    import matplotlib.pyplot as plt
+
+    fig, ax = plt.subplots()
+
+    ax.plot(means_combined, marker='o', label='combined')
+    ax.plot(means_cation, marker='o', label='cation')
+    ax.plot(means_anion, marker='o', label='anion')
+
+    ax.set_xticklabels(possible_n_qm)
+    ax.set_xlabel('# QM pairs')
+    ax.set_ylabel(r'distance ($\AA$)')
+
+    ax.legend(loc='best', fancybox=True, framealpha=0.50)
+
+    fig.savefig('box_sizes.pdf', bbox_inches='tight')
+
+    plt.close('all')
+
+    fig, ax = plt.subplots()
+
+    ax.plot(means_combined, possible_n_qm, marker='o', label='combined')
+    ax.plot(means_cation, possible_n_qm, marker='o', label='cation')
+    ax.plot(means_anion, possible_n_qm, marker='o', label='anion')
+
+    ax.set_xlabel(r'distance ($\AA$)')
+    ax.set_ylabel('# QM pairs')
+
+    ax.legend(loc='best', fancybox=True, framealpha=0.50)
+
+    fig.savefig('box_sizes_inverted.pdf', bbox_inches='tight')
